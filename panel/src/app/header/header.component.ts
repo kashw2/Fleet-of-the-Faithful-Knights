0615f334ec8a9f934ff41ace57ba8971460524e7
kashw2 <supra4keanu@hotmail.com>
2019-11-24 20:41:04 +1000

    removed introducing header tag

--- panel/src/app/header/header.component.ts
+++ panel/src/app/header/header.component.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit} from "@angular/core";
-import {None, Option} from "funfix-core";
-import {OptionUtils, User, UserJsonSerializer} from "../../../../core";
 import {LoginService} from "../../services/login.service";
 
 @Component({
-  selector: "app-header",
+  selector: "header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"]
 })
@@ -12,18 +10,8 @@ export class HeaderComponent implements OnInit {
 
   constructor(private loginService: LoginService) {
   }
-  loggedIn: Option<User> = None;
-
-  isLoggedIn(): boolean {
-    return this.loggedIn
-      .nonEmpty();
-  }
 
   ngOnInit() {
-    this.loginService.getUser()
-      .subscribe(user => {
-        this.loggedIn = OptionUtils.parseSerialised(user, UserJsonSerializer.instance);
-    });
   }
 
 }
