6656de603dc60d424e7fbe75c33656882b5d16fd
kashw2 <supra4keanu@hotmail.com>
2019-12-14 00:17:09 +1000

    bot login via environment variable

--- discord-bot/app.ts
+++ discord-bot/app.ts
@@ -1,13 +1,8 @@
 import * as discord from "discord.js";
-import {List} from "immutable";
 import {Events} from "./events/events";
 
-const clientToken = List(process.argv)
-    .filter(x => x.includes("FFK_DISCORD_BOT_SECRET"))
-    .map(x => x.substring(23))
-    .join();
 const client = new discord.Client();
 
 Events.startAllDiscordEvents(client);
 
-client.login(clientToken);
+client.login(process.env.FFK_DISCORD_BOT_SECRET);
--- discord-bot/events/message/message-event.ts
+++ discord-bot/events/message/message-event.ts
@@ -11,7 +11,7 @@ export class MessageEvent extends ClientEvents {
     initialiseEvent(): void {
         this.getClient()
             .on("message", message => {
-                console.log("Hello world");
+
             });
     }
 
