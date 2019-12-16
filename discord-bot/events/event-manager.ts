ae2f08a7a16b971513a94118a09663fb865ac5a7
kashw2 <supra4keanu@hotmail.com>
2019-12-16 20:27:43 +1000

    added some client user helper methods

--- discord-bot/events/event-manager.ts
+++ discord-bot/events/event-manager.ts
@@ -1,4 +1,5 @@
-import {Client} from "discord.js";
+import {Client, ClientUserSettings, DMChannel, Presence, Snowflake, User} from "discord.js";
+import {List} from "immutable";
 
 export abstract class EventManager {
 
@@ -9,10 +10,105 @@ export abstract class EventManager {
         return this.client;
     }
 
+    getClientAvatar(): string {
+        return this.getClient()
+            .user
+            .avatar;
+    }
+
+    getClientAvatarHref(): string {
+        return this.getClient()
+            .user
+            .avatarURL;
+    }
+
+    getClientBlocked(): List<User> {
+        return List(
+            this.getClient()
+                .user
+                .blocked
+                .values(),
+        );
+    }
+
+    getClientCreatedAt(): Date {
+        return this.getClient()
+            .user
+            .createdAt;
+    }
+
+    getClientCreationTimestamp(): number {
+        return this.getClient()
+            .user
+            .createdTimestamp;
+    }
+
+    getClientDefaultAvatarHref(): string {
+        return this.getClient()
+            .user
+            .defaultAvatarURL;
+    }
+
+    getClientDiscriminator(): string {
+        return this.getClient()
+            .user
+            .discriminator;
+    }
+
+    getClientDisplayAvatarHref(): string {
+        return this.getClient()
+            .user
+            .displayAvatarURL;
+    }
+
+    getClientEmail(): any {
+        return this.getClient()
+            .user
+            .email;
+    }
+
+    getClientFriends(): List<User> {
+        return List(
+            this.getClient()
+                .user
+                .friends
+                .values(),
+        );
+    }
+
+    getClientId(): Snowflake {
+        return this.getClient()
+            .user
+            .id;
+    }
+
+    getClientNote(): any {
+        return this.getClient()
+            .user
+            .note;
+    }
+
+    getClientPresence(): Presence {
+        return this.getClient()
+            .user
+            .presence;
+    }
+
+    getClientSettings(): ClientUserSettings {
+        return this.getClient()
+            .user
+            .settings;
+    }
+
     getClientTag(): string {
         return this.getClient()
             .user
             .tag;
+    }
+
+    getClientToken(): string {
+        return this.getClient()
+            .token;
     }
 
     getClientUsername(): string {
@@ -21,6 +117,48 @@ export abstract class EventManager {
             .username;
     }
 
+    getDmChannel(): DMChannel {
+        return this.getClient()
+            .user
+            .dmChannel;
+    }
+
+    getLastMessageId(): string {
+        return this.getClient()
+            .user
+            .lastMessageID;
+    }
+
     abstract initialiseEvent(): void;
+
+    isBot(): boolean {
+        return this.getClient()
+            .user
+            .bot;
+    }
+
+    isClientMfaEnabled(): boolean {
+        return this.getClient()
+            .user
+            .mfaEnabled;
+    }
+
+    isClientOnMobile(): boolean {
+        return this.getClient()
+            .user
+            .mobile;
+    }
+
+    isClientPremium(): boolean {
+        return this.getClient()
+            .user
+            .premium;
+    }
+
+    isClientVerified(): boolean {
+        return this.getClient()
+            .user
+            .verified;
+    }
 
 }
