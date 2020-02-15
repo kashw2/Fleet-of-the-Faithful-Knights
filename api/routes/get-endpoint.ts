b782b43c87cd60e662f594c9171dd104ea7a2e71
kashw2 <supra4keanu@hotmail.com>
2020-02-15 20:28:59 +1000

    added auth check

--- api/routes/get-endpoint.ts
+++ api/routes/get-endpoint.ts
@@ -7,25 +7,29 @@ export abstract class GetEndpoint extends RouteManager {
         super();
     }
 
+    routeEndpoint(router: Router): void {
+        router.get(this.getEndpoint(), ((req, res, next) => {
+            if (this.isAuthorized()) {
+                try {
+                    this.run(req, res);
+                } catch (exception) {
+                    res.sendStatus(500)
+                        .send(exception);
+                }
+            } else {
+                res.sendStatus(403);
+            }
+        }));
+    }
+
     isAuthorized(): boolean {
         return false;
     }
 
-    routeEndpoint(router: Router): void {
-        router.get(this.getEndpoint(), ((req, res, next) => {
-            try {
-                this.run(req, res);
-            } catch (exception) {
-                res.sendStatus(500)
-                    .send(exception);
-            }
-        }))
-    }
-
-    abstract run(req: Request, res: Response): void;
-
     private getEndpoint(): string {
         return this.endpoint;
     }
+
+    abstract run(req: Request, res: Response): void;
 
 }
