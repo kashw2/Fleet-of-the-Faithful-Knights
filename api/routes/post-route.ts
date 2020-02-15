c4e22f33139deb666910fb780f0a1a92b3a3e209
kashw2 <supra4keanu@hotmail.com>
2020-02-15 20:29:07 +1000

    added post endpoint class

--- api/routes/post-route.ts
+++ api/routes/post-route.ts
@@ -1,13 +1,34 @@
 import {RouteManager} from "./route-manager";
+import {Request, Response, Router} from "express";
 
-export class PostRoute extends RouteManager {
+export abstract class PostRoute extends RouteManager {
 
-    constructor(endpoint: string) {
+    constructor(private endpoint: string) {
         super();
+    }
+
+    routeEndpoint(router: Router): void {
+        router.post(this.getEndpoint(), ((req, res, next) => {
+            if (this.isAuthorized()) {
+                try {
+                    this.run(req, res);
+                } catch (exception) {
+                    res.sendStatus(500)
+                        .send(exception);
+                }
+            } else {
+                res.sendStatus(403);
+            }
+        }));
     }
 
     isAuthorized(): boolean {
         return false;
     }
 
+    abstract run(req: Request, res: Response): void;
+
+    private getEndpoint(): string {
+        return this.endpoint;
+    }
 }
