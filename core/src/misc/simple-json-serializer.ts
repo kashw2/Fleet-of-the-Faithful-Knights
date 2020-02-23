7ac31dd85df6959836d13985fd00f77c5bf64e92
kashw2 <supra4keanu@hotmail.com>
2020-02-23 15:32:14 +1000

    added serializable arrays

--- core/src/misc/simple-json-serializer.ts
+++ core/src/misc/simple-json-serializer.ts
@@ -1,8 +1,14 @@
 import {JsonBuilder} from "./json-builder";
+import {Collection, List} from "immutable";
 
 export abstract class SimpleJsonSerializer<T> {
 
     abstract fromJson(json: any): T;
+
+    fromJsonArray(collection: Collection<number, T>): List<T> {
+        return collection.map(x => this.fromJson(x))
+            .toList();
+    }
 
     toJson(value: T): object {
         return this.toJsonImpl(value, new JsonBuilder({}));
