5b3a0531ca9712bfe04b25c2b186ece18f2a3324
kashw2 <supra4keanu@hotmail.com>
2020-04-04 20:06:08 +1000

    json builder no longer takes constructor

--- core/src/misc/json-builder.ts
+++ core/src/misc/json-builder.ts
@@ -4,13 +4,12 @@ import {SimpleJsonSerializer} from "./simple-json-serializer";
 
 export class JsonBuilder {
 
-    constructor(private object: object) {
-    }
+    object: object = {};
 
     add<T>(value: any, key: string): JsonBuilder {
         // @ts-ignore
         this.object[key] = value;
-        return new JsonBuilder(this.object);
+        return new JsonBuilder();
     }
 
     // Don't know if this will work, will have to wait until i actually use it i guess
@@ -18,26 +17,26 @@ export class JsonBuilder {
     addList<T>(value: List<T>, key: string): JsonBuilder {
         if (!value.isEmpty()) {
             value.map((value, iteration) => this.object[key][iteration]);
-            return new JsonBuilder(this.object);
+            return new JsonBuilder();
         }
-        return new JsonBuilder(this.object);
+        return new JsonBuilder();
     }
 
     addOptional(value: Option<any>, key: string): JsonBuilder {
         if (!value.isEmpty()) {
             // @ts-ignore
             this.object[key] = value.get();
-            return new JsonBuilder(this.object);
+            return new JsonBuilder();
         }
-        return new JsonBuilder(this.object);
+        return new JsonBuilder();
     }
 
     addOptionalSerialized<T>(value: Option<T>, key: string, serializer: SimpleJsonSerializer<T>): JsonBuilder {
         if (!value.isEmpty()) {
             this.object[key] = serializer.toJson(value.get());
-            return new JsonBuilder(this.object);
+            return new JsonBuilder();
         }
-        return new JsonBuilder(this.object);
+        return new JsonBuilder();
     }
 
     build(): object {
