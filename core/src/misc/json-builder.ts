c7ca0c9ab29e5595106f42813da63d51a9a837c5
kashw2 <supra4keanu@hotmail.com>
2020-04-06 00:32:32 +1000

    reverted 5b3a0531ca9712bfe04b25c2b186ece18f2a3324

--- core/src/misc/json-builder.ts
+++ core/src/misc/json-builder.ts
@@ -4,39 +4,38 @@ import {SimpleJsonSerializer} from "./simple-json-serializer";
 
 export class JsonBuilder {
 
-    object: object = {};
+    constructor(private object: object = {}) {
+    }
 
     add<T>(value: any, key: string): JsonBuilder {
         // @ts-ignore
         this.object[key] = value;
-        return new JsonBuilder();
+        return new JsonBuilder(this.object);
     }
 
-    // Don't know if this will work, will have to wait until i actually use it i guess
-    // Possible FIXME?
-    addList<T>(value: List<T>, key: string): JsonBuilder {
-        if (!value.isEmpty()) {
-            value.map((value, iteration) => this.object[key][iteration]);
-            return new JsonBuilder();
+    addList<T>(values: List<T>, key: string): JsonBuilder {
+        if (!values.isEmpty()) {
+            values.map((value, iteration) => this.object[key][iteration]);
+            return new JsonBuilder(this.object);
         }
-        return new JsonBuilder();
+        return new JsonBuilder(this.object);
     }
 
     addOptional(value: Option<any>, key: string): JsonBuilder {
         if (!value.isEmpty()) {
             // @ts-ignore
             this.object[key] = value.get();
-            return new JsonBuilder();
+            return new JsonBuilder(this.object);
         }
-        return new JsonBuilder();
+        return new JsonBuilder(this.object);
     }
 
     addOptionalSerialized<T>(value: Option<T>, key: string, serializer: SimpleJsonSerializer<T>): JsonBuilder {
         if (!value.isEmpty()) {
             this.object[key] = serializer.toJson(value.get());
-            return new JsonBuilder();
+            return new JsonBuilder(this.object);
         }
-        return new JsonBuilder();
+        return new JsonBuilder(this.object);
     }
 
     build(): object {
