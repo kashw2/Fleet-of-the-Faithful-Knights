8a0c8159c22ac00f99fc952017910dca64b2976d
kashw2 <supra4keanu@hotmail.com>
2020-04-07 01:55:57 +1000

    added list votes by user endpoint

---
+++ api/routes/votes/list/list-votes-by-user-endpoint.ts
@@ -0,0 +1,32 @@
+import {Request, Response} from "express";
+import {Either} from "funfix-core";
+import {List} from "immutable";
+import {ApiUtils} from "../../../../core/src";
+import {Vote, VoteJsonSerializer} from "../../../../core/src/models/vote";
+import {Database} from "../../../db/database";
+import {GetRoute} from "../../get-route";
+
+export class ListVotesByUserEndpoint extends GetRoute {
+
+    constructor(private db: Database) {
+        super("/votes/user/:id");
+    }
+
+    private getUserId(req: Request): Either<string, number> {
+        return ApiUtils.parseNumberFromPath(req, "id");
+    }
+
+    private getVotesByUserId(userId: number): Either<string, List<Vote>> {
+        return this.db.cache.votes.getByUserId(userId);
+    }
+
+    isAuthorized(): boolean {
+        return true;
+    }
+
+    run(req: Request, res: Response): void {
+        this.getUserId(req)
+            .map(uid => ApiUtils.sendSerializedCollectionResult(this.getVotesByUserId(uid), VoteJsonSerializer.instance, res));
+    }
+
+}
