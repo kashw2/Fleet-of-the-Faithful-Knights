66aa61c20b071e6eb556abac589a30448dbc27f5
kashw2 <supra4keanu@hotmail.com>
2020-04-08 01:09:05 +1000

    added ffk api services

---
+++ panel/src/app/services/ffk-api/ffk-api-read.service.ts
@@ -0,0 +1,29 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class FfkApiReadService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getHostUrl(): string {
+    return "http://localhost:8080";
+  }
+
+  getUserByToken(token: string): Observable<object> {
+    return this.http.get(this.getHostUrl().concat(`/user/token/${token}`));
+  }
+
+  getVotes(): Observable<object> {
+    return this.http.get(this.getHostUrl().concat("/votes"));
+  }
+
+  getVotesByUser(userId: number): Observable<object> {
+    return this.http.get(this.getHostUrl().concat(`/votes/user/${userId}`));
+  }
+
+}
