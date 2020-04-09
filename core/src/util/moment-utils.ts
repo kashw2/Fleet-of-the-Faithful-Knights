4b4da69bfcbbc433b44b94eaf38c46fdc60dea95
kashw2 <supra4keanu@hotmail.com>
2020-04-10 02:07:51 +1000

    added date format stuff for moments

--- core/src/util/moment-utils.ts
+++ core/src/util/moment-utils.ts
@@ -1,10 +1,24 @@
 import * as moment from "moment";
 
+export type FfkDateFormat = "DMY" | "DMYHM";
+
 export class MomentUtils {
 
-    static formatString(s: string): string {
+    static formatString(s: string, format: FfkDateFormat): string {
         return moment(s)
-            .format("D/M/YY");
+            .format(this.getFfkDateFormatString(format));
+    }
+
+    static getFfkDateFormatString(format: FfkDateFormat): string {
+        switch (format) {
+            case "DMY":
+                return "D/M/YY";
+            case "DMYHM":
+                return "D/M/YY LT";
+            default:
+                console.info(`${format} is not a recognised FfkDateFormat, defaulting to DMY`);
+                return "D/M/YY LT";
+        }
     }
 
 }
