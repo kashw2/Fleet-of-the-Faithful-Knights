c453d2f36b51483fef5f723a8253442254065f6d
kashw2 <supra4keanu@hotmail.com>
2020-04-17 00:27:51 +1000

    added ngrx states

--- panel/src/app/app.module.ts
+++ panel/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-
 import {HttpClientModule} from "@angular/common/http";
+import {NgModule} from "@angular/core";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {RouterModule, Routes} from "@angular/router";
+import {StoreModule} from "@ngrx/store";
 import {MDBBootstrapModule} from "angular-bootstrap-md";
 import {CookieService} from "ngx-cookie-service";
 import {ToastrModule} from "ngx-toastr";
@@ -14,12 +14,15 @@ import {HeaderComponent} from "./components/header/header.component";
 import {CreateVoteModalComponent} from "./modals/create-vote-modal/create-vote-modal.component";
 import {ViewVoteModalComponent} from "./modals/view-vote-modal/view-vote-modal.component";
 import {ProfilePageComponent} from "./pages/profile-page/profile-page.component";
+import {SsoPageComponent} from "./pages/sso-page/sso-page.component";
 import {VotePageComponent} from "./pages/vote-page/vote-page.component";
 import {NotificationService} from "./services/notification.service";
+import {userReducer} from "./store/reducers/user-reducer";
 
 const routes: Routes = [
   {path: "", component: ProfilePageComponent},
   {path: "home", redirectTo: ""},
+  {path: "sso", component: SsoPageComponent},
   {path: "profile", component: ProfilePageComponent},
   {path: "votes", component: VotePageComponent},
 ];
@@ -33,6 +36,7 @@ const routes: Routes = [
     VotePageComponent,
     ViewVoteModalComponent,
     CreateVoteModalComponent,
+    SsoPageComponent,
   ],
   imports: [
     MDBBootstrapModule.forRoot(),
@@ -44,6 +48,9 @@ const routes: Routes = [
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
+    StoreModule.forRoot({
+      user: userReducer,
+    }),
   ],
   providers: [NotificationService, CookieService],
 })
