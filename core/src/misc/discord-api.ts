69773c3b6dcfc3b06f8bbcfc55f3d3a6e2b24272
kashw2 <supra4keanu@hotmail.com>
2020-05-07 17:33:18 +1000

    added new method and documentation

--- core/src/misc/discord-api.ts
+++ core/src/misc/discord-api.ts
@@ -2,7 +2,7 @@ import * as axios from "axios";
 import {Either, Left, None, Option, Right} from "funfix-core";
 import {List} from "immutable";
 import * as querystring from "querystring";
-import {accessTokenKey, expiresInKey, parseNumber, parseString, refreshTokenKey, scopeKey, tokenTypeKey,} from "..";
+import {accessTokenKey, expiresInKey, parseNumber, parseString, refreshTokenKey, scopeKey, tokenTypeKey} from "..";
 import {DiscordGuild, DiscordGuildJsonSerializer} from "../models/discord/discord-guild";
 import {DiscordGuildMember, DiscordGuildMemberJsonSerializer} from "../models/discord/discord-guild-member";
 import {DiscordUser, DiscordUserJsonSerilaizer} from "../models/discord/discord-user";
@@ -20,8 +20,13 @@ export class DiscordApi {
         return process.env.FFK_DISCORD_PANEL_BOT_TOKEN!;
     }
 
+    // Swap this for whatever guild
+    static getFfkGuildId(): string {
+        return "539188746114039818";
+    }
+
     static getGuildMember(userId: string, guildId: string, accessToken: string): Promise<Either<string, DiscordGuildMember>> {
-        return axios.default.get("https://discordapp.com/api/guilds/539188746114039818/members/178140794555727872", {
+        return axios.default.get(this.getDiscordApiUrl().concat(`/guilds/${guildId}/members/${userId}`), {
             headers: {
                 Authorization: `Bot ${this.getDiscordPanelBotToken()}`,
                 access_token: accessToken,
@@ -31,6 +36,35 @@ export class DiscordApi {
             .catch(x => Left(x));
     }
 
+    // This function take a guildId just so anytime it's called, we can tell by the params that we need to input a guild id.
+    // This is something that is intended to create ease in understanding of the codebase, some may not realise at first that you can set this using the getFfkGuildId function
+    static getGuildMembers(
+        guildId: string = this.getFfkGuildId(),
+        accessToken: string = this.getDiscordPanelBotToken(),
+        limit: number = 100,
+    ): Promise<Either<string, List<DiscordGuildMember>>> {
+        return axios.default.get(this.getDiscordApiUrl().concat(`/guilds/${guildId}/members?limit=${limit}`), {
+            headers: {
+                Authorization: `Bot ${this.getDiscordPanelBotToken()}`,
+                access_token: accessToken,
+            },
+        })
+            .then(x => Right(DiscordGuildMemberJsonSerializer.instance.fromJsonArray(List(x.data))))
+            .catch(x => Left(x));
+    }
+
+    /**
+     *
+     * Used to return an OAuthResponse containing the Access Code for other requests.
+     * Creates the foundation for usage of the Discord API
+     * NOTE: OAuth is only used for non-bot/user requests
+     *
+     * @param clientId the bot client id
+     * @param clientSecret the bot client secret
+     * @param code the code returned from discord to start the OAuth process
+     *
+     * @returns DiscordOAuthResponse
+     */
     static getOAuth(clientId: string, clientSecret: string, code: string): Promise<Either<string, DiscordOAuthResponse>> {
         return axios.default.post(this.getTokenRequestUrl(), querystring.encode({
             client_id: clientId,
