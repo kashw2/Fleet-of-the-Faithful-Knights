b891ead7d55b4a23a06ea07306a8ed5741af8a52
kashw2 <supra4keanu@hotmail.com>
2020-05-07 17:32:57 +1000

    fixed datamodel to take discord id as string

--- core/src/models/candidate.ts
+++ core/src/models/candidate.ts
@@ -1,4 +1,5 @@
 import {None, Option} from "funfix-core";
+import {List} from "immutable";
 import {
     discordIdKey,
     discordNameKey,
@@ -11,19 +12,34 @@ import {
     SimpleJsonSerializer,
 } from "..";
 import {Group, MiscUtil} from "../util/misc-util";
+import {DiscordGuildMember} from "./discord/discord-guild-member";
 
 export class Candidate {
 
     constructor(
         readonly id: Option<number> = None,
-        readonly discordId: Option<number> = None,
+        readonly discordId: Option<string> = None,
         readonly discordName: Option<string> = None,
         readonly group: Option<Group> = None,
         readonly memberSince: Option<string> = None,
     ) {
     }
 
-    public getDiscordId(): Option<number> {
+    static fromDiscordGuildMember(member: DiscordGuildMember): Candidate {
+        return new Candidate(
+            None,
+            member.getUser().flatMap(u => u.getId()),
+            member.getUser().flatMap(u => u.getUsername()),
+            MiscUtil.parseGroupOption(member.getRoles().first()),
+            None,
+        );
+    }
+
+    static fromDiscordGuildMembers(members: List<DiscordGuildMember>): List<Candidate> {
+        return members.map(m => this.fromDiscordGuildMember(m));
+    }
+
+    public getDiscordId(): Option<string> {
         return this.discordId;
     }
 
@@ -52,7 +68,7 @@ export class CandidateJsonSerializer extends SimpleJsonSerializer<Candidate> {
     fromJson(json: any): Candidate {
         return new Candidate(
             parseNumber(json[idKey]),
-            parseNumber(json[discordIdKey]),
+            parseString(json[discordIdKey]),
             parseString(json[discordNameKey]),
             MiscUtil.parseGroupOption(json[groupKey]),
             parseString(json[memberSinceKey]),
