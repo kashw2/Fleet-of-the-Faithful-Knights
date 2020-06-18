167805fea2d84926fed4645d9f62f5b1c91c69a6
kashw2 <supra4keanu@hotmail.com>
2020-06-18 23:30:05 +1000

    fixed leaks of keys

--- panel/src/environments/environment.prod.ts
+++ panel/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   FFK_API_ADDRESS: "http://localhost:8080",
-  FFK_API_TOKEN: "55E0C821-B8B1-4CC5-8770-C30D5FCEDFB7",
-  FFK_DISCORD_PANEL_BOT_TOKEN: "NjA3MDA1MDQzMDQzODYwNTIx.XoRiNg.oiWGj7UE4hcxAcCn_NS5fzLxcPk",
+  FFK_API_TOKEN: process.env.FFK_API_TOKEN,
+  FFK_DISCORD_PANEL_BOT_TOKEN: process.env.FFK_DISCORD_PANEL_BOT_TOKEN,
 };
