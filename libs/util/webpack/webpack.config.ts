0909ffbb84c4cd68e09b65d04576e24720731814
kashw2 <supra4keanu@hotmail.com>
2020-12-15 21:29:05 +1000

    added lib-util

---
+++ libs/util/webpack/webpack.config.ts
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: "./src/index.ts",
+    mode: "production",
+    devtool: "source-map",
+    target: "node",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    output: {
+        globalObject: "typeof self !== 'undefined' ? self : this", // See https://github.com/webpack/webpack/issues/6522
+        libraryTarget: "umd",
+        filename: 'lib-util.js'
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+        modules: ['src', 'node_modules']
+    }
+}
