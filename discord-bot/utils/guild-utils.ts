24257e347dcac9cb8e75b795591a0c1b8aa3a490
kashw2 <supra4keanu@hotmail.com>
2020-12-15 17:49:16 +1000

    rework starts

--- discord-bot/utils/guild-utils.ts
+++
@@ -1,9 +0,0 @@
-import {Guild, Message} from "discord.js";
-
-export class GuildUtils {
-
-    static getGuildFromMessage(message: Message): Guild {
-        return message.guild;
-    }
-
-}
--- onboarding/onboarding.ts
+++
@@ -1,9 +0,0 @@
-import {DiscordCandidateOnboarding} from "./candidate/discord/discord-candidate-onboarding";
-
-export class Onboarding {
-
-    static onboard(): void {
-        new DiscordCandidateOnboarding().onboard();
-    }
-
-}
