14829064e227b11a2100bc63690442e4da329afe
kashw2 <supra4keanu@hotmail.com>
2021-03-22 22:57:56 +1000

    fixed some cicd issues

--- panel/src/app/pages/home-page/home-page.component.ts
+++ panel/src/app/pages/home-page/home-page.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {None, Option, Some} from 'funfix-core';
 import {HyperlinkMap} from '@ffk/lib-angular';
 import {List, Set} from 'immutable';
-import {Group, News, User} from '@kashw2/lib-ts';
+import {News} from '@kashw2/lib-ts';
 import {CollectionUtils} from '@kashw2/lib-util';
 import * as moment from 'moment';
 import {UserService} from '../../service/user.service';
