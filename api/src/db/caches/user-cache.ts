ffb89848c7219665876ed4a907e9126eec993eb6
kashw2 <supra4keanu@hotmail.com>
2021-04-22 20:30:54 +1000

    added abstraction layer for caching

--- api/src/db/caches/user-cache.ts
+++ api/src/db/caches/user-cache.ts
@@ -1,18 +1,16 @@
 import {List} from "immutable";
 import {User} from "@kashw2/lib-ts";
+import {Cache} from "./cache";
 
-export class UserCache {
+export class UserCache extends Cache<User> {
 
     // TODO: Make this a Map so we can get by user id, user name etc
     constructor(private users: List<User> = List()) {
+        super(users);
     }
 
     getUsers(): List<User> {
         return this.users;
-    }
-
-    update(users: List<User>): List<User> {
-        return this.users = this.users.clear().concat(users);
     }
 
 }
