991caf4610925242158c88442309c753a848a597
kashw2 <supra4keanu@hotmail.com>
2021-04-24 11:09:29 +1000

    lint

--- libs/util/src/json-serializer.ts
+++ libs/util/src/json-serializer.ts
@@ -5,37 +5,41 @@ import {OptionUtils} from './option-utils';
 
 export abstract class JsonSerializer<A> {
 
-	abstract fromJson(json: any): A;
+    abstract fromJson(json: any): A;
 
-	fromJsonArray<B>(objs: List<object>): List<A> {
-		return OptionUtils.flattenList(objs.map(v => Option.of(v)))
-			.map(v => this.fromJson(v));
+    public fromJsonArray<B>(objs: List<object>): List<A> {
+        return OptionUtils.flattenList(objs.map(v => Option.of(v)))
+            .map(v => this.fromJson(v));
 
-	}
+    }
 
-	public fromJsonImpl(json: any): Option<A> {
-		return Option.of(json)
-			.filter(v => typeof v === 'object')
-			.filter(v => v !== {})
-			.map(v => this.fromJson(v));
-	}
+    public fromJsonImpl(json: any): Option<A> {
+        return Option.of(json)
+            .filter(v => typeof v === 'object')
+            .filter(v => v !== {})
+            .map(v => this.fromJson(v));
+    }
 
-	fromJsonString(json: string): A {
-		return this.fromJson(JSON.parse(json));
-	}
+    public fromJsonString(json: string): A {
+        return this.fromJson(JSON.parse(json));
+    }
 
-	abstract toJson(value: A, builder: JsonBuilder): Record<string, any>;
+    abstract toJson(value: A, builder: JsonBuilder): Record<string, any>;
 
-	public toJsonArray(values: Collection<any, A>, builder: JsonBuilder): Collection<any, Record<string, any>> {
-		return values.map(v => this.toJson(v, builder));
-	}
+    public toJsonArray(values: Collection<any, A>, builder: JsonBuilder): Collection<any, Record<string, any>> {
+        return values.map(v => this.toJson(v, builder));
+    }
 
-	public toJsonImpl(value: A): any {
-		return this.toJson(value, new JsonBuilder());
-	}
+    public toJsonArrayImpl(value: Collection<any, A>): any {
+        return value.map(v => this.toJsonImpl(v));
+    }
 
-	toJsonString(value: A): string {
-		return JSON.stringify(this.toJson(value, new JsonBuilder()));
-	}
+    public toJsonImpl(value: A): any {
+        return this.toJson(value, new JsonBuilder());
+    }
+
+    toJsonString(value: A): string {
+        return JSON.stringify(this.toJson(value, new JsonBuilder()));
+    }
 
 }
