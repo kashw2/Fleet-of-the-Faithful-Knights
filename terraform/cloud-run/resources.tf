789735dbb2bc4dcf572ef686278ff3f8c402013b
kashw2 <supra4keanu@hotmail.com>
2021-04-28 20:47:22 +1000

    added terraform for api

---
+++ terraform/cloud-run/resources.tf
@@ -0,0 +1,41 @@
+resource "google_cloud_run_service" "api" {
+  location = var.region
+  name = "api"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/the-sandbox-311907/api:latest"
+        ports {
+          container_port = 3000
+        }
+        env {
+          name = "FFK_DATABASE_SERVER"
+          value = var.FFK_DATABASE_SERVER
+        }
+        env {
+          name = "FFK_DATABASE_PORT"
+          value = var.FFK_DATABASE_PORT
+        }
+        env {
+          name = "FFK_DATABASE_NAME"
+          value = var.FFK_DATABASE_NAME
+        }
+        env {
+          name = "FFK_DATABASE_USERNAME"
+          value = var.FFK_DATABASE_USERNAME
+        }
+        env {
+          name = "FFK_DATABASE_PASSWORD"
+          value = var.FFK_DATABASE_PASSWORD
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent = 100
+    latest_revision = true
+  }
+
+}
---
+++ terraform/cloud-run/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "FFK_DATABASE_SERVER" {}
+
+variable "FFK_DATABASE_PORT" {}
+
+variable "FFK_DATABASE_NAME" {}
+
+variable "FFK_DATABASE_USERNAME" {}
+
+variable "FFK_DATABASE_PASSWORD" {}
