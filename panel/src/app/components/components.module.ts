0a42d79778384844a822a3dc919437b27159ad2a
kashw2 <supra4keanu@hotmail.com>
2021-05-08 23:40:20 +1000

    fix: removed old panel stuff

--- panel/src/app/components/components.module.ts
+++ panel/src/app/components/components.module.ts
@@ -1,27 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ArticleComponent} from './article/article.component';
-import {ColouredTextComponent} from './coloured-text/coloured-text.component';
-import {HeaderComponent} from './header/header.component';
-import {SafeTdComponent} from './safe-td/safe-td.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
+import {SidebarComponent} from "./sidebar/sidebar.component";
 
 
 
 @NgModule({
   declarations: [
-    ArticleComponent,
-    ColouredTextComponent,
-    HeaderComponent,
-    SafeTdComponent,
-    DropdownComponent,
+    SidebarComponent
   ],
   exports: [
-    ArticleComponent,
-    ColouredTextComponent,
-    HeaderComponent,
-    SafeTdComponent,
-    DropdownComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule
