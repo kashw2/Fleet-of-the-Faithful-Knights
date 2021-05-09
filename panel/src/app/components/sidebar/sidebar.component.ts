59a45175d0936feb56bf4d395eb49b35d0b3ac5e
kashw2 <supra4keanu@hotmail.com>
2021-05-09 22:57:38 +1000

    feat: added tooltips

--- panel/src/app/components/sidebar/sidebar.component.ts
+++ panel/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavigationService} from "../../service/navigation.service";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(readonly navigationService: NavigationService) { }
 
   ngOnInit(): void {
   }
