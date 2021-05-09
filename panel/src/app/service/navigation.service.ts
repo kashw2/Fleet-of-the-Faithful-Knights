daeeb2fe7e7f285c3fd8079fba56cee542bfd003
kashw2 <supra4keanu@hotmail.com>
2021-05-09 22:57:19 +1000

    feat: added navigation service

---
+++ panel/src/app/service/navigation.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationService {
+
+  constructor(private router: Router) { }
+
+  home(): void {
+    return this.navigate('home');
+  }
+
+  navigate(route: string): void {
+    if (!this.router.url.includes(route)) {
+      console.log('Navigating to', route);
+      this.router.navigate([route])
+        .then(v => console.log('Navigated to', route))
+        .catch(err => console.error('Error navigating to', route, err));
+      return;
+    }
+    console.info(`Navigation request rejected ${this.router.url} = /${route}`);
+    return;
+  }
+
+}
