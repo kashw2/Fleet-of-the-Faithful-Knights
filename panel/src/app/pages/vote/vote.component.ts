b807d2d5ca7966376ed4a0e722b484dc958992ca
kashw2 <supra4keanu@hotmail.com>
2021-06-25 00:06:00 +1000

    feature: filter votes by selected option

--- panel/src/app/pages/vote/vote.component.ts
+++ panel/src/app/pages/vote/vote.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {List} from "immutable";
-import {Option, Some} from "funfix-core";
+import {None, Option, Some} from "funfix-core";
 import {VoteService} from "../../service/vote.service";
+import {BehaviorSubject} from "rxjs";
+import {Group, Vote} from "@kashw2/lib-ts";
+import {UserService} from "../../service/user.service";
 
 @Component({
   selector: 'app-vote',
@@ -10,7 +13,17 @@ import {VoteService} from "../../service/vote.service";
 })
 export class VoteComponent implements OnInit {
 
-  constructor(readonly voteService: VoteService) {
+  constructor(
+    readonly voteService: VoteService,
+    private userService: UserService,
+  ) {
+  }
+
+  selectedGroup: BehaviorSubject<Option<string>> = new BehaviorSubject<Option<string>>(None);
+
+  getFilteredVotes(): List<Vote> {
+    return this.voteService.getVotes()
+      .filter(v => v.getPromotionGroupName().equals(this.getSelectedGroup()));
   }
 
   getGroupLabels(): List<Option<string>> {
@@ -30,7 +43,31 @@ export class VoteComponent implements OnInit {
     );
   }
 
+  getSelectedGroup(): Option<string> {
+    return this.selectedGroup
+      .getValue();
+  }
+
+  private getUserGroup(): Option<Group> {
+    return this.userService
+      .getUser()
+      .flatMap(u => u.getGroup());
+  }
+
   ngOnInit(): void {
+  }
+
+  setSelectedGroup(group: Option<string>): Option<string> {
+    if (group.isEmpty()) {
+      return this.getSelectedGroup();
+    }
+    if (this.getSelectedGroup().equals(group)) {
+      this.selectedGroup.next(None);
+      return this.getSelectedGroup();
+    }
+    console.info(`Setting Group to ${group.get()}`);
+    this.selectedGroup.next(group);
+    return group;
   }
 
 }
