b63e81cef7064587a6280e0079ff9f3aad42503a
kashw2 <supra4keanu@hotmail.com>
2021-06-30 12:30:48 +1000

    feature(toast-service): reworked service and added new functionality

--- panel/src/app/service/toast.service.ts
+++ panel/src/app/service/toast.service.ts
@@ -1,6 +1,10 @@
 import {Injectable} from '@angular/core';
-import {Either, Option} from 'funfix-core';
+import {Either} from 'funfix-core';
 import {ToastrService} from 'ngx-toastr';
+import {List} from "immutable";
+import {EitherUtils} from "@kashw2/lib-util";
+import {Observable, zip} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -14,56 +18,89 @@ export class ToastService {
    *
    */
 
-  constructor(private toastr: ToastrService) {
+  constructor(private toastrService: ToastrService) {
   }
 
-  private showError(
-    s: string,
-    title: string = 'Error',
-    duration: number = 3000,
+  show(
+    message: string,
+    type: 'Error' | 'Warning' | 'Info' | 'Success',
+    title: string = type,
+    timeout: number = 1500,
   ): void {
-    this.toastr.error(s, title);
-  }
-
-  private showInfo(
-    s: string,
-    title: string = 'Info',
-    duration: number = 2500,
-  ): void {
-    this.toastr.info(s, title);
-  }
-
-  private showSuccess(
-    s: string,
-    title: string = 'Success',
-    duration: number = 1500,
-  ): void {
-    this.toastr.success(s, title);
-  }
-
-  showToast(s: string, type: 'Success' | 'Error' | 'Warning' | 'Info' = 'Info', duration: number, title: string = type): void {
     switch (type) {
-    case 'Success':
-      return this.showSuccess(s, title, duration);
-    case 'Error':
-      return this.showError(s, title, duration);
-    case 'Warning':
-      return this.showWarning(s, title, duration);
-    case 'Info':
-      return this.showInfo(s, title, duration);
+      case 'Error':
+        return this.showError(message, title, timeout);
+      case 'Warning':
+        return this.showWarning(message, title, timeout);
+      case 'Info':
+        return this.showInfo(message, title, timeout);
+      case 'Success':
+        return this.showSuccess(message, title, timeout);
     }
   }
 
-  showToastEither(e: Either<string, string>): void {
-    return e.isRight() ? this.showSuccess(e.get()) : this.showError(e.value);
+  showAndRecoverList<A>(
+    input: Either<string, List<A>>,
+    success: string = '',
+    lType: 'Error' | 'Warning' = 'Error',
+    rType: 'Success' | 'Info' = 'Info',
+    title: string = input.isRight() ? rType : lType,
+  ): List<A> {
+    if (input.isLeft()) {
+      EitherUtils.leftTap(input, v => this.showError(v, title));
+      return List<A>();
+    }
+    if (success.length > 0) {
+      this.showSuccess(success, title);
+    }
+    return input.get();
   }
 
-  showToastOption(o: Option<string>): void {
-    return o.nonEmpty() ? this.showSuccess(o.get()) : this.showError('Error');
+  showAndRecoverListObservable<A>(
+    input: Observable<Either<string, List<A>>>,
+    success: string = '',
+    lType: 'Error' | 'Warning' = 'Error',
+    rType: 'Success' | 'Info' = 'Info',
+    title: Observable<string> = input.pipe(map(v => v.isRight() ? rType : lType)),
+  ): Observable<List<A>> {
+    return zip(input, title)
+      .pipe(map(([either, label]) => this.showAndRecoverList<A>(either, label)));
   }
 
-  private showWarning(s: string, title: string = 'Warning', duration: number = 2750): void {
-    this.toastr.warning(s, title);
+  private showError(message: string, title: string = 'Error!', timeout: number = 2500): void {
+    this.toastrService.error(message, title, {
+      timeOut: timeout,
+      progressBar: true,
+      enableHtml: true,
+      newestOnTop: true
+    });
+  }
+
+  private showInfo(message: string, title: string = 'Info', timeout: number = 5000): void {
+    this.toastrService.info(message, title, {
+      timeOut: timeout,
+      progressBar: true,
+      enableHtml: true,
+      newestOnTop: true,
+    });
+  }
+
+  private showSuccess(message: string, title: string = 'Success!', timeout: number = 1750): void {
+    this.toastrService.success(message, title, {
+      timeOut: timeout,
+      progressBar: true,
+      enableHtml: true,
+      newestOnTop: true,
+    });
+  }
+
+  private showWarning(message: string, title: string = 'Warning!', timeout: number = 3000): void {
+    this.toastrService.warning(message, title, {
+      timeOut: timeout,
+      progressBar: true,
+      enableHtml: true,
+      newestOnTop: true,
+    });
   }
 
 }
