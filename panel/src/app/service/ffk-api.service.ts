78071fbfcbb76d21a003211b15efc8fe529a2d13
kashw2 <supra4keanu@hotmail.com>
2021-07-01 01:38:16 +1000

    feature(panel): created FfkApiService

---
+++ panel/src/app/service/ffk-api.service.ts
@@ -0,0 +1,48 @@
+import {Inject, Injectable} from '@angular/core';
+import {CrudApiBase, Group, GroupJsonSerializer, User, UserJsonSerializer} from "@kashw2/lib-ts";
+import {Either} from "funfix-core";
+import {List} from "immutable";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FfkApiService {
+
+  constructor(
+    @Inject('ffkApiServer') private apiServer: string,
+    @Inject('ffkDiscordId') private discordId: string,
+  ) {
+  }
+
+  permission: CrudApiBase = new CrudApiBase(this.apiServer, 'permission');
+
+  getDiscordId(): string {
+    return this.discordId;
+  }
+
+  getGroups(): Promise<Either<string, List<Group>>> {
+    return this.group()
+      .sendReadRequestList(
+        GroupJsonSerializer.instance,
+        {},
+        {'Discord-Id': this.getDiscordId()}
+      );
+  }
+
+  getUser(): Promise<Either<string, User>> {
+    return this.user(this.getDiscordId())
+      .sendReadRequest(
+        UserJsonSerializer.instance,
+        {},
+        {
+          'Discord-Id': this.getDiscordId(),
+          'Access-Control-Allow-Origin': '*'
+        }
+      )
+  }
+
+  group: (gid?: number) => CrudApiBase = (gid?: number) => new CrudApiBase(this.apiServer, gid ? `group?group_id=${gid}` : 'group');
+  user: (uid: string) => CrudApiBase = (uid: string) => new CrudApiBase(this.apiServer, uid ? `user?user_id=${uid}` : 'user');
+  userPermissionMapping: (uid: string) => CrudApiBase = (uid: string) => new CrudApiBase(this.apiServer, `user/${uid}/permission/mapping`);
+
+}
