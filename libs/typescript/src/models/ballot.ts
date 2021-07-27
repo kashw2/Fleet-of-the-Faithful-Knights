48809515e8a1ef8bc9223d657ab608cc7d1d7531
kashw2 <supra4keanu@hotmail.com>
2021-07-28 00:41:27 +1000

    chore(ballot): changed response type to number

--- libs/typescript/src/models/ballot.ts
+++ libs/typescript/src/models/ballot.ts
@@ -1,4 +1,4 @@
-import {JsonBuilder, JsonSerializer, MomentUtils, parseDate, parseString} from '@kashw2/lib-util';
+import {JsonBuilder, JsonSerializer, MomentUtils, parseDate, parseNumber, parseString} from '@kashw2/lib-util';
 import {None, Option, Some} from 'funfix-core';
 import {User, UserJsonSerializer} from './user';
 import * as moment from 'moment';
@@ -6,110 +6,114 @@ import {createdKey, descriptionKey, idKey, modifiedKey, responseKey, voterKey} f
 
 export class Ballot {
 
-	constructor(
-		private id: Option<string> = None,
-		private voter: Option<User> = None,
-		private description: Option<string> = None,
-		private response: Option<string> = None,
-		private created: Option<moment.Moment> = None,
-		private modified: Option<moment.Moment> = None,
-	) {
-	}
+    constructor(
+        private id: Option<string> = None,
+        private voter: Option<User> = None,
+        private description: Option<string> = None,
+        private response: Option<number> = None,
+        private created: Option<moment.Moment> = None,
+        private modified: Option<moment.Moment> = None,
+    ) {
+    }
 
-	public getCreated(): Option<moment.Moment> {
-		return this.created;
-	}
+    public getCreated(): Option<moment.Moment> {
+        return this.created;
+    }
 
-	public getCreatedFormatted(format: 'DMY'): Option<string> {
-		return MomentUtils.format(this.getCreated(), format);
-	}
+    public getCreatedFormatted(format: 'DMY'): Option<string> {
+        return MomentUtils.format(this.getCreated(), format);
+    }
 
-	public getDescription(): Option<string> {
-		return this.description;
-	}
+    public getDescription(): Option<string> {
+        return this.description;
+    }
 
-	public getFormattedVoterDiscordAvatar(): Option<string> {
-		return Option.map2(
-			this.getVoterDiscordId(),
-			this.getVoterDiscordAvatar(),
-			(did, avatar) => `https://cdn.discordapp.com/avatars/${did}/${avatar}.png`,
-		);
-	}
+    public getFormattedVoterDiscordAvatar(): Option<string> {
+        return Option.map2(
+            this.getVoterDiscordId(),
+            this.getVoterDiscordAvatar(),
+            (did, avatar) => `https://cdn.discordapp.com/avatars/${did}/${avatar}.png`,
+        );
+    }
 
-	public getId(): Option<string> {
-		return this.id;
-	}
+    public getId(): Option<string> {
+        return this.id;
+    }
 
-	public getModified(): Option<moment.Moment> {
-		return this.modified;
-	}
+    public getModified(): Option<moment.Moment> {
+        return this.modified;
+    }
 
-	public getModifiedFormatted(format: 'DMY'): Option<string> {
-		return MomentUtils.format(this.getModified(), format);
-	}
+    public getModifiedFormatted(format: 'DMY'): Option<string> {
+        return MomentUtils.format(this.getModified(), format);
+    }
 
-	public getPreparedResponse(): Option<string> {
-		if (this.response.contains('Y')) {
-			return Some('Affirm');
-		} else if (this.response.contains('N')) {
-			return Some('Deny');
-		}
-		return None;
-	}
+    public getPreparedResponse(): Option<string> {
+        if (this.getResponse().contains(0)) {
+            return Some('Affirm');
+        }
+        if (this.getResponse().contains(1)) {
+            return Some('Deny');
+        }
+        if (this.getResponse().contains(2)) {
+            return Some('Veto');
+        }
+        return None;
+    }
 
-	public getResponse(): Option<string> {
-		return this.response;
-	}
+    public getResponse(): Option<number> {
+        return this.response;
+    }
 
-	public getVoter(): Option<User> {
-		return this.voter;
-	}
+    public getVoter(): Option<User> {
+        return this.voter;
+    }
 
-	public getVoterDiscordAvatar(): Option<string> {
-		return this.getVoter()
-			.flatMap(v => v.getAvatar());
-	}
+    public getVoterDiscordAvatar(): Option<string> {
+        return this.getVoter()
+            .flatMap(v => v.getAvatar());
+    }
 
-	public getVoterDiscordId(): Option<string> {
-		return this.getVoter()
-			.flatMap(v => v.getDiscordId());
-	}
+    public getVoterDiscordId(): Option<string> {
+        return this.getVoter()
+            .flatMap(v => v.getDiscordId());
+    }
 
-	public getVoterId(): Option<string> {
-		return this.getVoter()
-			.flatMap(v => v.getId());
-	}
+    public getVoterId(): Option<string> {
+        return this.getVoter()
+            .flatMap(v => v.getId());
+    }
 
-	public getVoterUsername(): Option<string> {
-		return this.getVoter()
-			.flatMap(v => v.getUsername());
-	}
+    public getVoterUsername(): Option<string> {
+        return this.getVoter()
+            .flatMap(v => v.getUsername());
+    }
 
 }
 
 export class BallotJsonSerializer extends JsonSerializer<Ballot> {
 
-	static instance: BallotJsonSerializer = new BallotJsonSerializer();
+    static instance: BallotJsonSerializer = new BallotJsonSerializer();
 
-	fromJson(json: any): Ballot {
-		return new Ballot(
-			parseString(json[idKey]),
-			UserJsonSerializer.instance.fromJsonImpl(json[voterKey]),
-			parseString(json[descriptionKey]),
-			parseString(json[responseKey]),
-			parseDate(json[createdKey]),
-			parseDate(json[modifiedKey]),
-		);
-	}
+    fromJson(json: any): Ballot {
+        return new Ballot(
+            parseString(json[idKey]),
+            UserJsonSerializer.instance.fromJsonImpl(json[voterKey]),
+            parseString(json[descriptionKey]),
+            parseNumber(json[responseKey]),
+            parseDate(json[createdKey]),
+            parseDate(json[modifiedKey]),
+        );
+    }
 
-	toJson(value: Ballot, builder: JsonBuilder): Record<string, any> {
-		return builder.addOptional(value.getId(), idKey)
-			.addOptionalSerialized(value.getVoter(), voterKey, UserJsonSerializer.instance)
-			.addOptional(value.getDescription(), descriptionKey)
-			.addOptional(value.getResponse(), responseKey)
-			.addOptional(value.getCreated(), createdKey)
-			.addOptional(value.getModified(), modifiedKey)
-			.build();
-	}
+    toJson(value: Ballot, builder: JsonBuilder): Record<string, any> {
+        return builder.addOptional(value.getId(), idKey)
+            .addOptionalSerialized(value.getVoter(), voterKey, UserJsonSerializer.instance)
+            .addOptional(value.getDescription(), descriptionKey)
+            .addOptional(value.getResponse(), responseKey)
+            .addOptional(value.getCreated(), createdKey)
+            .addOptional(value.getModified(), modifiedKey)
+            .build();
+    }
 
 }
