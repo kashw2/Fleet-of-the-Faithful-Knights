898c9658d3a57ac73ebf429747b985513ce22794
kashw2 <supra4keanu@hotmail.com>
2021-07-28 01:33:49 +1000

    feature(ballot): created ballot caching

---
+++ api/src/db/caches/ballot-cache.ts
@@ -0,0 +1,23 @@
+import {Cache} from "./cache";
+import {Ballot} from "@kashw2/lib-ts";
+import {List, Map} from "immutable";
+import {CollectionUtils, EitherUtils} from "@kashw2/lib-util";
+import {Either, Option} from "funfix-core";
+
+export class BallotCache extends Cache<Ballot> {
+
+    constructor(private ballots: List<Ballot> = List()) {
+        super(ballots);
+    }
+
+    byId: Map<string, Ballot> = CollectionUtils.buildKeyedMap(this.getBallots(), (g) => g.getId());
+
+    getBallotById(ballotId: string): Either<string, Ballot> {
+        return EitherUtils.toEither(Option.of(this.byId.get(ballotId)), `Ballot with id ${ballotId} does not exist`);
+    }
+
+    getBallots(): List<Ballot> {
+        return super.getValues();
+    }
+
+}
