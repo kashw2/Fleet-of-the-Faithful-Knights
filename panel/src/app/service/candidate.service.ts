613067e1429448acba5fb7731e11cc2ad6b102f6
kashw2 <supra4keanu@hotmail.com>
2021-08-11 22:09:06 +1000

    feature(candidate): pull live candidates from api

--- panel/src/app/service/candidate.service.ts
+++ panel/src/app/service/candidate.service.ts
@@ -1,28 +1,32 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {Candidate, Group} from '@kashw2/lib-ts';
-import {List, Set} from 'immutable';
-import {Either, Option, Right, Some} from 'funfix-core';
-import {StarCitizenUser} from '@kashw2/lib-external';
-import * as moment from 'moment';
+import {Candidate} from '@kashw2/lib-ts';
+import {List} from 'immutable';
+import {Option} from 'funfix-core';
 import {ToastService} from "./toast.service";
+import {FfkApiService} from "./ffk-api.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidateService {
 
-  constructor(private toastService: ToastService) {
+  constructor(
+    private toastService: ToastService,
+    private ffkApiService: FfkApiService,
+    ) {
+    this.ffkApiService.getCandidates()
+      .then(g => this.setCandidates(this.toastService.showAndRecoverList(g, `Loaded ${g.getOrElse(List()).size} Candidates`)));
   }
 
-  private candidates: BehaviorSubject<Either<string, List<Candidate>>> = new BehaviorSubject<Either<string, List<Candidate>>>(this.getDefaultCandidates());
+  private candidates: BehaviorSubject<List<Candidate>> = new BehaviorSubject<List<Candidate>>(List());
 
-  asObs(): Observable<Either<string, List<Candidate>>> {
+  asObs(): Observable<List<Candidate>> {
     return this.candidates;
   }
 
   clear(): void {
-    return this.candidates.next(Right(List()));
+    return this.candidates.next(List());
   }
 
   getCandidate(index: number): Option<Candidate> {
@@ -31,41 +35,13 @@ export class CandidateService {
 
   getCandidates(): List<Candidate> {
     return this.candidates.getValue()
-      .getOrElse(List());
-  }
-
-  getDefaultCandidates(): Either<string, List<Candidate>> {
-    return Right(List.of(
-      new Candidate(
-        Some('1'),
-        Some('Bship'),
-        Some('280932387716726786'),
-        Some('#5578'),
-        Some('5c9a69a8ed4d1130447d8bf0a5685461'),
-        Some(new Group(Some('1'), Some('Master Commander'), Some('#ff0000'))),
-        Some(new StarCitizenUser(
-          Some('123456'),
-          Some('Bship'),
-          Some('Bship'),
-          Some('Bship'),
-          Some(moment()),
-          Some('Colorado'),
-          Some('English'),
-          Some('faithfulknights.com'),
-          Some('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ducimus molestiae porro quas. Autem delectus dicta, dolores doloribus et hic in incidunt possimus provident quos similique sunt veniam veritatis voluptatum. Alias aliquid animi corporis deserunt distinctio enim hic ipsa laborum, minus mollitia nulla omnis quas quasi\n' +
-            '  quidem, sed sequi similique, suscipit totam vitae voluptate! A aliquid aspernatur blanditiis delectus dolor. Accusantium ad aliquid architecto culpa dolores ea earum eos esse eum facilis harum in inventore ipsa iure laborum\n' +
-            '  natus quae quasi, quos recusandae rem rerum unde ut voluptatem! Officia, officiis.'),
-          Set.of(),
-        )),
-      ),
-    ));
   }
 
   setCandidates(candidates: List<Candidate>): List<Candidate> {
     if (candidates.isEmpty() || this.getCandidates().equals(candidates)) {
       return this.getCandidates();
     }
-    this.candidates.next(Right(candidates));
+    this.candidates.next(candidates);
     return candidates;
   }
 
