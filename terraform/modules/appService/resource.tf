847c5430973de0cb514ebbecbd98472cf724c062
kashw2 <supra4keanu@hotmail.com>
2021-08-18 22:52:27 +1000

    feature(operations): added api app service

--- terraform/modules/appService/resource.tf
+++ terraform/modules/appService/resource.tf
@@ -4,3 +4,10 @@ resource "azurerm_app_service" "panel" {
   name = "voting-panel"
   resource_group_name = var.resource_group_name
 }
+
+resource "azurerm_app_service" "api" {
+  app_service_plan_id = var.app_service_plan_id
+  location = var.resource_group_location
+  name = "api"
+  resource_group_name = var.resource_group_name
+}
