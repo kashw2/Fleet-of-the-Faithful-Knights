ed61a8a5b8ddc353a34fe06b0e304e152a4a44ae
kashw2 <supra4keanu@hotmail.com>
2021-08-21 00:10:31 +1000

    chore(operations): backend changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,23 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 2.46.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 2.73.0"
     }
     azuread = {
-      source = "hashicorp/azuread"
+      source  = "hashicorp/azuread"
       version = "~> 1.6.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
+  }
+  backend "azurerm" {
+    storage_account_name = "ffktfstorageaccount"
+    resource_group_name  = "tf_resource_group"
+    container_name       = "terraform-state-container"
+    key                  = "terraform.tfstate"
   }
 }
 
@@ -20,6 +26,7 @@ provider "azurerm" {
   subscription_id = "e608fd59-f3db-4b75-9dfb-ba60889b45f3"
 }
 
-provider "azuread" {}
+provider "azuread" {
+}
 
 provider "random" {}
