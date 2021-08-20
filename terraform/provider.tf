001dda83a9fa99ea9210d2d41e1a3960777735b3
kashw2 <supra4keanu@hotmail.com>
2021-08-21 00:21:14 +1000

    chore(operations): removed unused providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,14 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 2.73.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 1.6.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
   }
   backend "azurerm" {
     storage_account_name = "ffktfstorageaccount"
@@ -25,8 +17,3 @@ provider "azurerm" {
   features {}
   subscription_id = "e608fd59-f3db-4b75-9dfb-ba60889b45f3"
 }
-
-provider "azuread" {
-}
-
-provider "random" {}
