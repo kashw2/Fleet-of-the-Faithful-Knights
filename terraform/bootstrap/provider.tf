f3292ac1e1c82566e33514bf5f9832601352a482
kashw2 <supra4keanu@hotmail.com>
2021-08-21 00:06:56 +1000

    chore(operations): added bootstrap

---
+++ terraform/bootstrap/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.73.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "e608fd59-f3db-4b75-9dfb-ba60889b45f3"
+}
+
+provider "azuread" {
+}
+
+provider "random" {}
