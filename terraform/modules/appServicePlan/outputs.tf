739b3a8338caeef0206c1db0b8763b20270834c8
kashw2 <supra4keanu@hotmail.com>
2021-08-21 14:08:32 +1000

    chore(app-service-plan): migrated service plan to linux

--- terraform/modules/appServicePlan/outputs.tf
+++ terraform/modules/appServicePlan/outputs.tf
@@ -1,3 +1,3 @@
 output "id" {
-  value = azurerm_app_service_plan.app_service_asp.id
+  value = azurerm_app_service_plan.app_service_plan.id
 }
--- terraform/modules/appServicePlan/resource.tf
+++ terraform/modules/appServicePlan/resource.tf
@@ -1,7 +1,11 @@
-resource "azurerm_app_service_plan" "app_service_asp" {
+resource "azurerm_app_service_plan" "app_service_plan" {
   location            = var.resource_group_location
-  name                = "ffk-free-app-service-plan"
+  name                = "ffk-basic-app-service-plan"
   resource_group_name = var.resource_group_name
+
+  kind     = "Linux"
+  reserved = true
+
   sku {
     size = "B1"
     tier = "Basic"
