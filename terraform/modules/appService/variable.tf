f101356cbcacfe39db6ff3eaaf33e9a976f81725
kashw2 <supra4keanu@hotmail.com>
2021-08-21 21:15:34 +1000

    ci: enabled prod builds during deployment

--- terraform/modules/appService/variable.tf
+++ terraform/modules/appService/variable.tf
@@ -4,4 +4,24 @@ variable "resource_group_location" {}
 
 variable "resource_group_name" {}
 
-variable "container_registry_login_server" {}
+variable "login_server" {}
+
+variable "username" {}
+
+variable "password" {}
+
+variable "FFK_DATABASE_USERNAME" {}
+
+variable "FFK_DATABASE_PASSWORD" {}
+
+variable "FFK_DATABASE_NAME" {}
+
+variable "FFK_DATABASE_SERVER" {}
+
+variable "FFK_DATABASE_PORT" {}
+
+variable "FFK_DISCORD_CLIENT_SECRET" {}
+
+variable "FFK_DISCORD_REDIRECT" {}
+
+variable "FFK_DISCORD_BOT_TOKEN" {}
