b0f6710e0e6253ba79cc62027809eb1001419dd4
kashw2 <supra4keanu@hotmail.com>
2021-08-29 23:08:09 +1000

    refactor: optimised initial api requests

--- panel/src/app/pages/home/home.component.ts
+++ panel/src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {Option} from "funfix-core";
+import {Either, Left, Option, Right} from "funfix-core";
 import {ActivatedRoute} from "@angular/router";
 import {FfkApiService} from "../../service/ffk-api.service";
-import {distinctUntilChanged, filter, from, map, of, pipe, switchMap, takeUntil, takeWhile, tap} from "rxjs";
+import {distinctUntilChanged, filter, from, map, of, pipe, switchMap, takeUntil, takeWhile, tap, zip} from "rxjs";
 import {CrudService} from "../../service/crud.service";
 import {UserService} from "../../service/user.service";
 import {ToastService} from "../../service/toast.service";
@@ -10,6 +10,7 @@ import {List} from "immutable";
 import {GroupService} from "../../service/group.service";
 import {CandidateService} from "../../service/candidate.service";
 import {VoteService} from "../../service/vote.service";
+import {Vote} from "@kashw2/lib-ts";
 
 @Component({
   selector: 'app-home',
@@ -44,33 +45,41 @@ export class HomeComponent implements OnInit {
         .pipe(filter(v => v.nonEmpty()))
         .pipe(map(v => v.get()))
         .pipe(tap(did => this.crudService.crudLocalStorageService.create('discordid', did)))
-        // Get Groups
-        .pipe(switchMap(_ => from(this.ffkApiService.getGroups())))
-        .pipe(tap(gs => this.groupService.setGroups(this.toastService.showAndRecoverList(gs))))
-        // Get Candidates
-        .pipe(switchMap(_ => from(this.ffkApiService.getCandidates())))
-        .pipe(tap(cs => this.candidateService.setCandidates(this.toastService.showAndRecoverList(cs, `Loaded ${cs.getOrElse(List()).size} Candidates`))))
-        // Get Votes
-        .pipe(switchMap(_ => from(this.ffkApiService.getVotes())))
-        .pipe(tap(vs => this.voteService.setVotes(this.toastService.showAndRecoverList(vs, `Loaded ${vs.getOrElse(List()).size} Votes`))))
+        .pipe(switchMap(_ => zip(
+          this.ffkApiService.getGroups(),
+          this.ffkApiService.getCandidates(),
+          this.ffkApiService.getVotes(),
+        )))
+        .pipe(tap(([groups, candidates, votes]) => {
+          this.groupService.setGroups(this.toastService.showAndRecoverList(groups));
+          this.candidateService.setCandidates(this.toastService.showAndRecoverList(candidates, `Loaded ${candidates.getOrElse(List()).size} Candidates`));
+          this.voteService.setVotes(this.toastService.showAndRecoverList(votes, `Loaded ${votes.getOrElse(List()).size} Votes`));
+        }))
         .subscribe();
     } else {
       this.userService.asObs()
         .pipe(switchMap((v) => {
-            if (v.nonEmpty()) {
-              return from(this.ffkApiService.getGroups())
-                // Get Groups
-                .pipe(switchMap(_ => from(this.ffkApiService.getGroups())))
-                .pipe(switchMap(_ => from(this.ffkApiService.getGroups())))
-                .pipe(tap(gs => this.groupService.setGroups(this.toastService.showAndRecoverList(gs))))
-                // Get Candidates
-                .pipe(switchMap(_ => from(this.ffkApiService.getCandidates())))
-                .pipe(tap(cs => this.candidateService.setCandidates(this.toastService.showAndRecoverList(cs, `Loaded ${cs.getOrElse(List()).size} Candidates`))))
-                // Get Votes
-                .pipe(switchMap(_ => from(this.ffkApiService.getVotes())))
-                .pipe(tap(vs => this.voteService.setVotes(this.toastService.showAndRecoverList(vs, `Loaded ${vs.getOrElse(List()).size} Votes`))));
+            // If we're logged in and we haven't got data in the respective states
+            if (
+              v.nonEmpty() &&
+              (
+                this.groupService.getGroups().isEmpty()
+                || this.candidateService.getCandidates().isEmpty()
+                || this.voteService.getVotes().isEmpty()
+              )
+            ) {
+              return zip(
+                this.ffkApiService.getGroups(),
+                this.ffkApiService.getCandidates(),
+                this.ffkApiService.getVotes(),
+              )
+                .pipe(tap(([groups, candidates, votes]) => {
+                  this.groupService.setGroups(this.toastService.showAndRecoverList(groups));
+                  this.candidateService.setCandidates(this.toastService.showAndRecoverList(candidates, `Loaded ${candidates.getOrElse(List()).size} Candidates`));
+                  this.voteService.setVotes(this.toastService.showAndRecoverList(votes, `Loaded ${votes.getOrElse(List()).size} Votes`));
+                }));
             }
-            return of();
+            return of<Either<string, List<Vote>>>(Right(List()));
           }
         ))
         .subscribe(console.warn);
