e35f53ed2a6882c6af7098e012436ccff16befd4
kashw2 <supra4keanu@hotmail.com>
2021-09-04 01:35:48 +1000

    feature: navigate home if guard fail

--- panel/src/app/guards/candidate.guard.ts
+++ panel/src/app/guards/candidate.guard.ts
@@ -1,22 +1,31 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
-import {every, Observable, of, switchMap} from 'rxjs';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {every, Observable, of, switchMap, tap} from 'rxjs';
 import {CandidateService} from "../service/candidate.service";
+import {NavigationService} from "../service/navigation.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidateGuard implements CanActivate {
 
-  constructor(private candidateService: CandidateService) {
+  constructor(
+    private candidateService: CandidateService,
+    private navigationService: NavigationService,
+  ) {
   }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  ): Observable<boolean> {
     return of(false)
-      .pipe(switchMap(_ => this.hasCandidates()));
+      .pipe(switchMap(_ => this.hasCandidates()))
+      .pipe(tap(v => {
+        if (!v) {
+          this.navigationService.home();
+        }
+      }));
   }
 
   hasCandidates(): Observable<boolean> {
--- panel/src/app/guards/vote.guard.ts
+++ panel/src/app/guards/vote.guard.ts
@@ -1,14 +1,18 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
-import {every, Observable, of, switchMap} from 'rxjs';
+import {every, Observable, of, switchMap, tap} from 'rxjs';
 import {VoteService} from "../service/vote.service";
+import {NavigationService} from "../service/navigation.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteGuard implements CanActivate {
 
-  constructor(private voteService: VoteService) {
+  constructor(
+    private voteService: VoteService,
+    private navigationService: NavigationService,
+  ) {
   }
 
   canActivate(
@@ -16,7 +20,12 @@ export class VoteGuard implements CanActivate {
     state: RouterStateSnapshot,
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return of(false)
-      .pipe(switchMap(_ => this.hasVotes()));
+      .pipe(switchMap(_ => this.hasVotes()))
+      .pipe(tap(v => {
+        if (!v) {
+          this.navigationService.home();
+        }
+      }));
   }
 
   hasVotes(): Observable<boolean> {
