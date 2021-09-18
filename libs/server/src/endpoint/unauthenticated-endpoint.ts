46d874a3714d73480b4311c8bb566e8a58287f22
kashw2 <supra4keanu@hotmail.com>
2021-09-18 15:51:01 +1000

    feature: added unauthenticated endpoints

---
+++ libs/server/src/endpoint/unauthenticated-endpoint.ts
@@ -0,0 +1,12 @@
+import {ApiEndpoint} from "./api-endpoint";
+import {Request, Response} from "express";
+
+export abstract class UnauthenticatedEndpoint extends ApiEndpoint {
+
+    constructor(readonly endpoint: string) {
+        super(endpoint);
+    }
+
+    abstract runImpl(req: Request, res: Response): void;
+
+}
