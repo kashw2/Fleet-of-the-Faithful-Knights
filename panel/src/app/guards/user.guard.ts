91997dc7c6b92359dda52ac6ed310037debea666
kashw2 <supra4keanu@hotmail.com>
2021-10-07 22:02:43 +1000

    feature: added user guard
    Signed-off-by: kashw2 <supra4keanu@hotmail.com>

---
+++ panel/src/app/guards/user.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {every, map, Observable, of, switchMap, tap} from 'rxjs';
+import {UserService} from "../service/user.service";
+import {NavigationService} from "../service/navigation.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+
+  constructor(
+    private userService: UserService,
+    private navigationService: NavigationService,
+    ) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return of(false)
+      .pipe(switchMap(_ => this.userService.asObs()))
+      .pipe(map(u => u.nonEmpty()))
+      .pipe(tap(v => {
+        if (!v) {
+          this.navigationService.home();
+        }
+      }));
+  }
+
+}
