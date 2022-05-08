91beeb25e0e095923e6c1d6ba5add7c6fe6fdda8
kashw2 <supra4keanu@hotmail.com>
2022-05-08 21:56:14 +1000

    fix(api): imported api endpoints
    Signed-off-by: kashw2 <supra4keanu@hotmail.com>

--- api/src/endpoints/all-endpoints.ts
+++ api/src/endpoints/all-endpoints.ts
@@ -2,15 +2,20 @@ import {Router} from "express";
 import {Database} from "../db/database";
 import {BallotEndpoint} from "./ballot-endpoint";
 import {CandidateEndpoint} from "./candidate-endpoint";
+import {UserPermissionMappingEndpoint} from "./user-permission-mapping-endpoint";
+import {PermissionsEndpoint} from "./permission-endpoint";
+import {GroupEndpoint} from "./group-endpoint";
+import {VotesEndpoint} from "./votes-endpoint";
+import {UserEndpoint} from "./user-endpoint";
 
 export class AllEndpoints {
 
     static initialiseEndpoints(router: Router, db: Database): void {
-        // new UserEndpoint(db).mount(router);
-        // new GroupEndpoint(db).mount(router);
-        // new PermissionsEndpoint(db).mount(router);
-        // new UserPermissionMappingEndpoint(db).mount(router);
-        // new VotesEndpoint(db).mount(router);
+        new UserEndpoint(db).mount(router);
+        new GroupEndpoint(db).mount(router);
+        new PermissionsEndpoint(db).mount(router);
+        new UserPermissionMappingEndpoint(db).mount(router);
+        new VotesEndpoint(db).mount(router);
         new BallotEndpoint(db).mount(router);
         new CandidateEndpoint(db).mount(router);
     }
