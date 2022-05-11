16f1618fb0b1b527b04f0e0708838568d015eeba
kashw2 <supra4keanu@hotmail.com>
2022-05-11 11:06:16 +1000

    chore(panel): migrated to futures
    Signed-off-by: kashw2 <supra4keanu@hotmail.com>

--- panel/src/app/service/ffk-api.service.ts
+++ panel/src/app/service/ffk-api.service.ts
@@ -1,20 +1,10 @@
 import {Inject, Injectable} from '@angular/core';
-import {
-  Ballot,
-  BallotJsonSerializer,
-  Candidate,
-  CandidateJsonSerializer,
-  Group,
-  GroupJsonSerializer,
-  User,
-  UserJsonSerializer,
-  Vote,
-  VoteJsonSerializer
-} from "@kashw2/lib-ts";
-import {Either} from "funfix-core";
+import {Ballot, BallotJsonSerializer, Candidate, CandidateJsonSerializer, Group, GroupJsonSerializer, User, UserJsonSerializer, Vote, VoteJsonSerializer} from "@kashw2/lib-ts";
+import {Either, Left} from "funfix-core";
 import {List} from "immutable";
 import {CrudLocalStorageService} from "./crud.service";
 import {CrudApiBase, EitherUtils} from "@kashw2/lib-util";
+import {Future} from "funfix";
 
 
 @Injectable({
@@ -35,8 +25,8 @@ export class FfkApiService {
 
   ballot: (vid?: string) => CrudApiBase = (vid?: string) => new CrudApiBase(this.apiServer, `ballot?vote_id=${vid}`);
 
-  deleteUser(uid: string): Promise<Either<any, User>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  deleteUser(): Future<Either<any, User>> {
+    return this.getDiscordId()
       .map(did => this.user(did, true)
         .sendDeleteRequest(
           UserJsonSerializer.instance,
@@ -45,11 +35,12 @@ export class FfkApiService {
             'Access-Control-Allow-Origin': '*'
           },
           {},
-        )));
+        ))
+      .getOrElse(Future.pure(Left('Unable to delete User')));
   }
 
-  getBallots(): Promise<Either<string, List<Ballot>>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  getBallots(): Future<List<Ballot>> {
+    return this.getDiscordId()
       .map(did => this.ballot()
         .sendReadRequestList(
           BallotJsonSerializer.instance,
@@ -58,11 +49,11 @@ export class FfkApiService {
             'Access-Control-Allow-Origin': '*'
           },
           {},
-        )));
+        )).getOrElse(Future.pure(List<Ballot>()));
   }
 
-  getCandidates(): Promise<Either<string, List<Candidate>>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  getCandidates(): Future<List<Candidate>> {
+    return this.getDiscordId()
       .map(did => this.candidate
         .sendReadRequestList(
           CandidateJsonSerializer.instance,
@@ -71,15 +62,15 @@ export class FfkApiService {
             'Access-Control-Allow-Origin': '*'
           },
           {},
-        )));
+        )).getOrElse(Future.pure(List<Candidate>()));
   }
 
   getDiscordId(): Either<string, string> {
     return EitherUtils.toEither(this.crudLocalStorageService.read('discordid'), "Client -> Server Handshake Failed");
   }
 
-  getGroups(): Promise<Either<string, List<Group>>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  getGroups(): Future<List<Group>> {
+    return this.getDiscordId()
       .map(did => this.group()
         .sendReadRequestList(
           GroupJsonSerializer.instance,
@@ -88,11 +79,11 @@ export class FfkApiService {
             'Access-Control-Allow-Origin': '*'
           },
           {},
-        )));
+        )).getOrElse(Future.pure(List<Group>()));
   }
 
-  getUser(): Promise<Either<string, User>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  getUser(): Future<Either<string, User>> {
+    return this.getDiscordId()
       .map(did => this.user(did).sendReadRequest(
         UserJsonSerializer.instance,
         {
@@ -100,11 +91,11 @@ export class FfkApiService {
           'Access-Control-Allow-Origin': '*'
         },
         {},
-      )));
+      )).getOrElse(Future.pure(Left('Unable to return User')));
   }
 
-  getUsers(): Promise<Either<string, List<User>>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  getUsers(): Future<List<User>> {
+    return this.getDiscordId()
       .map(did => this.user().sendReadRequestList(
         UserJsonSerializer.instance,
         {
@@ -112,11 +103,11 @@ export class FfkApiService {
           'Access-Control-Allow-Origin': '*'
         },
         {},
-      )));
+      )).getOrElse(Future.pure(List<User>()));
   }
 
-  getVotes(): Promise<Either<string, List<Vote>>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  getVotes(): Future<List<Vote>> {
+    return this.getDiscordId()
       .map(did => this.vote
         .sendReadRequestList(
           VoteJsonSerializer.instance,
@@ -125,15 +116,15 @@ export class FfkApiService {
             'Access-Control-Allow-Origin': '*'
           },
           {},
-        )));
+        )).getOrElse(Future.pure(List<Vote>()));
   }
 
   group: (gid?: number) => CrudApiBase = (gid?: number) => new CrudApiBase(this.apiServer, gid ? `group?group_id=${gid}` : 'group');
   user: (uid?: string, discord?: boolean) => CrudApiBase = (uid: string, discord: boolean) => new CrudApiBase(this.apiServer, discord ? `user?discord_token=${uid}` : uid ? `user?user_id=${uid}` : 'user');
   userPermissionMapping: (uid: string) => CrudApiBase = (uid: string) => new CrudApiBase(this.apiServer, `user/${uid}/permission/mapping`);
 
-  writeBallot(ballot: Ballot, voteId: string): Promise<Either<string, Ballot>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  writeBallot(ballot: Ballot, voteId: string): Future<Either<string, Ballot>> {
+    return this.getDiscordId()
       .map(did => this.ballot(voteId)
         .sendCreateRequest(
           BallotJsonSerializer.instance,
@@ -144,16 +135,16 @@ export class FfkApiService {
           {
             ballot: BallotJsonSerializer.instance.toJsonImpl(ballot)
           },
-        )));
+        )).getOrElse(Future.pure(Left('Unable to write Ballot')));
   }
 
-  writeUser(code: string): Promise<Either<string, User>> {
+  writeUser(code: string): Future<Either<string, User>> {
     return this.user(code, true)
       .sendCreateRequest(UserJsonSerializer.instance, {'Access-Control-Allow-Origin': '*'});
   }
 
-  writeVote(vote: Vote): Promise<Either<string, Vote>> {
-    return EitherUtils.sequence(this.getDiscordId()
+  writeVote(vote: Vote): Future<Either<string, Vote>> {
+    return this.getDiscordId()
       .map(did => this.vote
         .sendCreateRequest(
           VoteJsonSerializer.instance,
@@ -164,7 +155,7 @@ export class FfkApiService {
           {
             vote: VoteJsonSerializer.instance.toJsonImpl(vote)
           },
-        )));
+        )).getOrElse(Future.pure(Left('Unable to write Vote')));
   }
 
 }
