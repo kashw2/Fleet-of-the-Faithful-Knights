9a78dd4868989c53e5b48ef09428fc66ea210d28
kashw2 <supra4keanu@hotmail.com>
2023-08-18 14:34:32 +1000

    fix(nix): added some variables

--- shell.nix
+++ shell.nix
@@ -1,6 +1,17 @@
 { pkgs ? import <nixpkgs> {}}:
 
 pkgs.mkShell {
+
+    FFK_API_SERVER = "http://localhost:3000";
+    FFK_DISCORD_CLIENT_SECRET = "REDACTED";
+    FFK_DISCORD_BOT_TOKEN = "REDACTED";
+    FFK_DISCORD_REDIRECT = "http://localhost:4200";
+    FFK_DATABASE_USERNAME = "sa";
+    FFK_DATABASE_PASSWORD = "password123";
+    FFK_DATABASE_NAME = "Fleet_of_the_Faithful_Knights";
+    FFK_DATABASE_SERVER = "localhost";
+    FFK_DATABASE_PORT = 1433;
+
     packages = [
         pkgs.nodejs-18_x
         pkgs.act
